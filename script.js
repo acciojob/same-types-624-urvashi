d13eb17c98cf9935b526ce24758905d6888dac72
624-urvashi <143706170+624-urvashi@users.noreply.github.com>
2024-12-27 02:02:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function isSameType(value1, value2) {
 		return false;
 	return typeof value1=== typeof value2
 }
+}
 console.log(isSameType(1, 2)); // true
 console.log(isSameType(1, 'a')); // false
 console.log(isSameType(NaN, NaN)); // true
-console.log(isSameType(NaN, 1)); // fals
-}
+console.log(isSameType(NaN, 1)); // false
 
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
